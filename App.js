793c0d4c369d5b43b573e5f7f0bfc178ce2a9d03
Shriya003 <84763562+Shriya003@users.noreply.github.com>
2021-09-08 23:32:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+import EmojiData from './emoji.json';
+
+function App() {
+  const [search, setSearch] = useState('')
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const newData = EmojiData.filter(emoji => emoji.name.toLowerCase().includes(search.toLowerCase()));
+    setData(newData);
+  }, [search])
+
+  return (
+    <div>
+      <center>
+        <h1>😂😜Emoji Search😊🤩</h1>
+        <input type="text" name="search" value={search} onChange={(e) => setSearch(e.target.value)} />
+      </center>
+      {data.map(emoji => <div>
+
+        <div class="card">
+          <div class="card-body">
+            {emoji.symbol}{emoji.data}
+          </div>
+        </div>
+      </div>)}
+
+    </div>
+  );
+}
+
+export default App;
